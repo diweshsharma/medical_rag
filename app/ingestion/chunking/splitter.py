0126982c0ec804fb72c17baf3ed128a2c5eeaec2
diweshsharma <diweshsharma.janu@gmail.com>
2026-09-10 17:45:19 +0530

    splitter

---
+++ app/ingestion/chunking/splitter.py
@@ -0,0 +1,33 @@
+from anthropic import __name
+import logfire 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def chunking(docs):
+    """load documents and split them into chunks """
+    with logfire.span("chunking"):
+        splitter = RecursiveCharacterTextSplitter(
+            seperator =["\n\n" , "\n" , " ", ""],
+            chunk_size = 1000,
+            chunk_overlap = 200,
+
+        )
+
+        all_chunks = []
+        for doc in docs:
+            splits = splitter.split_text(doc)
+
+            for i, split_text in enumerate(splits):
+                chunk_metadata = dict[doc.metadata]
+                chunk_metadata['chunk_index'] = i
+                chunk_metadata["chunk_id"] = f"{doc.metadata.get('id')}_{i}"
+                chunk_metadata["total_chunks"] = len(splits)
+
+                all_chunks.append({
+                    "page_content": split_text,
+                    "metadata": chunk_metadata,
+                    
+                })
+        logfire.info("Number of chunks", len(all_chunks))
+        return all_chunks
+        
+
